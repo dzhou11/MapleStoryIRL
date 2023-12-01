62f43a71b00acb53c80f4d6d0a328decb647a1bc
dzhou11 <dzhou11@hotmail.com>
2023-12-01 00:53:53 -0500

    init

---
+++ src/main/java/com/dzhou11/maplestoryIRL/MaplestoryIrlApplication.java
@@ -0,0 +1,13 @@
+package com.dzhou11.maplestoryIRL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplestoryIrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaplestoryIrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dzhou11/maplestoryIRL/MaplestoryIrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzhou11.maplestoryIRL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaplestoryIrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
